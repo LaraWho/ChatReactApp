e1c307e04cec8f9ae2da44bebc990ddb908b8681
Lara Potjewyd <lara@Laras-MacBook-Pro.local>
2018-08-03 14:49:40 -0600

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post';
 
 class App extends Component {
   constructor() {
@@ -13,29 +14,51 @@ class App extends Component {
       posts: []
     };
 
+    this.baseUrl = 'https://practiceapi.devmountain.com/api'
+
     this.updatePost = this.updatePost.bind( this );
     this.deletePost = this.deletePost.bind( this );
     this.createPost = this.createPost.bind( this );
   }
   
   componentDidMount() {
-
+    axios.get(`${this.baseUrl}/posts`).then(results => {
+      this.setState({ posts: results.data })
+    })
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios.put(`${this.baseUrl}/posts/${id}/${text}`).then(results => {
+      this.setState({ posts: results.data })
+    })
   }
 
-  deletePost() {
-
+  deletePost(id, text) {
+    axios.delete(`${this.baseUrl}/posts/${id}`).then(results => {
+      this.setState({ posts: results.data })
+    })
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios.post(`${this.baseUrl}/posts`, {text}).then(results => {
+      this.setState({ posts: results.data})
+    })
   }
 
   render() {
-    const { posts } = this.state;
+    const posts = this.state.posts.map((post, i) => {
+      return(
+        <Post 
+        key={post.id}
+        text={post.text}
+        date={post.date}
+        id={post.id}
+        updatePostFn={this.updatePost}
+        deletePost={this.deletePost}
+        />
+      )
+    })
+  
 
     return (
       <div className="App__parent">
@@ -43,8 +66,10 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
-          
+          <Compose 
+          createPostFn={ this.createPost }/>
+          { posts }
+
         </section>
       </div>
     );
--- src/components/Compose/Compose.js
+++ src/components/Compose/Compose.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ProfileIcon from 'react-icons/lib/md/person-outline';
-
+import axios from 'axios';
 import './Compose.css';
 
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *APP* COMPONENT
@@ -21,7 +21,10 @@ export default class Compose extends Component {
   }
 
   createPost() {
-
+    const { text } = this.state;
+    const { createPostFn } = this.props;
+    createPostFn(text);
+    this.setState({ text: '' })
   }
 
   render() {
